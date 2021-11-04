2a520e58641210d6e286b767b1993e5f4a918f1f
Benjamin Altmiks <benjamin.altmiks@hs-augsburg.de>
2021-11-04 12:15:24 +0100

    Deleted some so far unnecessary files

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hallo Lukas!
-        </p>
-      </header>
-    </div>
-  );
+const App: React.FC = () => {
+    return (
+        <div>
+            Hallo Lukas
+        </div>
+    )
 }
 
 export default App;
