dd92d18bc7e54332495af552d6dc6ab52079d444
Lukas Kleybolte <lukas.kleybolte@hs-augsburg.de>
2021-11-12 01:29:24 +0100

    add regierungsbezirke bayern and add hover animation

--- src/components/IntactiveMap.tsx
+++ src/components/IntactiveMap.tsx
@@ -1,11 +1,12 @@
-import { GeoJsonObject } from "geojson";
 import regierungsbezirke from "../data/regierungsbezirke.json";
-import { FC, useState } from "react";
-import { MapContainer, GeoJSON, GeoJSONProps } from "react-leaflet";
-import { GeoJSONEvent } from "leaflet";
+import { Component, FC, useState } from "react";
+import { MapContainer, GeoJSON, useMap } from "react-leaflet";
+import { Feature, GeoJsonObject, GeometryObject } from "geojson";
+import { Layer, LeafletEvent, Map as LeafletMap } from "leaflet";
+import "leaflet/dist/leaflet.css";
 
-const InteractiveMap: FC = () => {
-    const countryStyle = {
+class InteractiveMap extends Component {
+    countryStyle = {
         fillColor: "red",
         fillOpacity: 0.5,
         color: "white",
@@ -13,19 +14,42 @@ const InteractiveMap: FC = () => {
     };
 
     // eslint-disable-next-line
-    const onEachFeature = (feature: any, layer: any) => {
+    onEachFeature = (feature: Feature<GeometryObject, any>, layer: Layer) => {
         const name = feature.properties.NAME_2;
         layer.bindPopup(name);
+        layer.on({
+            mouseover: (event: LeafletEvent) => {
+                event.target.setStyle({
+                    fillColor: "red",
+                    fillOpacity: 0.75,
+                    color: "white",
+                    weight: 3,
+                });
+                layer.openPopup();
+            },
+            mouseout: (event: LeafletEvent) => {
+                event.target.setStyle({
+                    fillColor: "red",
+                    fillOpacity: 0.5,
+                    color: "white",
+                    weight: 1,
+                });
+                layer.closePopup();
+            },
+        });
     };
 
-    return (
-        <div id={"MapWrapper"}>
-            <h1 style={{ textAlign: "center" }}>Test Map</h1>
-            <MapContainer zoom={6} style={{ height: "80vh" }} center={[51, 10]}>
-                <GeoJSON style={countryStyle} data={regierungsbezirke as GeoJsonObject} onEachFeature={onEachFeature} />
+    render() {
+        return (
+            <MapContainer zoom={7} style={{ height: "100vh", width: "100%" }} center={[48.7, 12]}>
+                <GeoJSON
+                    style={this.countryStyle}
+                    data={regierungsbezirke as GeoJsonObject}
+                    onEachFeature={this.onEachFeature}
+                />
             </MapContainer>
-        </div>
-    );
-};
+        );
+    }
+}
 
 export default InteractiveMap;
