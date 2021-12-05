c38087ed0dcd00d87f3196359f75e07061da40e7
Lukas <Lukas.Kleybolte@HS-Augsburg.de>
2021-12-05 22:56:51 +0100

    fix @typescript-eslint/no-unused-vars error
    Signed-off-by: Lukas <Lukas.Kleybolte@HS-Augsburg.de>

--- src/components/ScrollComponent.tsx
+++ src/components/ScrollComponent.tsx
@@ -1,4 +1,4 @@
-import { createRef, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Scrollama, Step } from "react-scrollama";
 import ScrollStepComponent from "./ScrollStepComponent";
 import "../style/ScrollComponent.scss";
