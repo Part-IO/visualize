4365ecf0d55af3b5a56b514ef159b257edcbd4b0
Benjamin Altmiks <benjamin.altmiks@hs-augsburg.de>
2021-12-30 11:33:49 +0100

    close #26 - delete scrollama and define new layout

--- src/components/ScrollComponent.tsx
+++ src/components/ScrollComponent.tsx
@@ -1,55 +1,20 @@
-import { useEffect, useState } from "react";
-import { Scrollama, Step } from "react-scrollama";
-import ScrollStepComponent from "./ScrollStepComponent";
 import "../style/ScrollComponent.scss";
 import TimeLineComponent from "./TimeLineComponent";
-import scroller, { ScrollerObserver } from "../utils/Scroller";
-import { districts } from "../utils/Helper";
 import StackedBarComponent from "./StackedBarComponent";
+import ScrollStepComponent from "./ScrollStepComponent";
+import DataLoader from "../utils/DataLoader";
 
 interface IState {
     data: string;
-    steps: string[];
-    progress: number;
+    districts: string[];
+    govermentDistricts: string[];
 }
 
 const ScrollComponent = (props: { mapComponent: JSX.Element }): JSX.Element => {
     const initialState: IState = {
         data: "",
-        steps: districts,
-        progress: 0,
-    };
-
-    const [getState, setGetState] = useState<IState>(initialState);
-
-    const onStateChange: ScrollerObserver = (district: string) => {
-        setGetState((state: IState) => {
-            return { ...state, data: district };
-        });
-    };
-
-    useEffect(() => {
-        scroller.attach(onStateChange);
-        return () => scroller.detach(onStateChange);
-    });
-
-    const onStepEnter = (e) => {
-        const { data } = e;
-        scroller.onScrollStateChange(data);
-    };
-
-    const onStepExit = ({ direction, data }) => {
-        if (direction === "up" && data === getState.steps[0]) {
-            setGetState((state: IState) => {
-                return { ...state, data: "" };
-            });
-        }
-    };
-
-    const onStepProgress = ({ progress }) => {
-        setGetState((state: IState) => {
-            return { ...state, progress: progress };
-        });
+        districts: [],
+        govermentDistricts: [],
     };
 
     return (
@@ -60,25 +25,10 @@ const ScrollComponent = (props: { mapComponent: JSX.Element }): JSX.Element => {
                         <ScrollStepComponent />
                     </div>
                     <div className="scroller">
-                        <Scrollama
-                            onStepEnter={onStepEnter}
-                            onStepExit={onStepExit}
-                            progress
-                            order
-                            onStepProgress={onStepProgress}
-                            offset="0.23"
-                        >
-                            {getState.steps.map((value: string) => {
-                                return (
-                                    <Step data={value} key={value}>
-                                        <div id={value + "-view"} className={"step"}>
-                                            <p>{value}</p>
-                                            <StackedBarComponent />
-                                        </div>
-                                    </Step>
-                                );
-                            })}
-                        </Scrollama>
+                        <div className={"step"}>
+                            <p>Hallo</p>
+                            <StackedBarComponent />
+                        </div>
                     </div>
                     <div className="graphic">
                         <div className="map">{props.mapComponent}</div>
